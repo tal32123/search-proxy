650e7f6edf5764df88ad5bc62e5bef41ac658d6e
Tal Charnes <tal32123@gmail.com>
2024-06-22 11:24:28 +0300

    feat: add paging to api on frontend

--- frontend/services/api.service.ts
+++ frontend/services/api.service.ts
@@ -6,23 +6,22 @@ const api = axios.create({
   baseURL: 'http://localhost:3001',
 });
 
-export const search = async (query: string): Promise<SearchResponseDto[]> => {
-    try {
-      const response = await api.get<SearchResponseDto[]>(`/search?q=${query}`);
-      return response.data;
-    } catch (error) {
-      console.error('Search API Error:', error);
-      throw error;
-    }
-  };
-  
+export const search = async (query: string, page: number = 1, pageSize: number = 2): Promise<SearchResponseDto[]> => {
+  try {
+    const response = await api.get<SearchResponseDto[]>(`/search?q=${query}&page=${page}&pageSize=${pageSize}`);
+    return response.data;
+  } catch (error) {
+    console.error('Search API Error:', error);
+    throw error;
+  }
+};
 
-  export const fetchHistory = async (): Promise<HistoryItem[]> => {
-    try {
-      const response = await api.get('/history');
-      return response.data;
-    } catch (error) {
-      console.error('Fetch History API Error:', error);
-      throw error;
-    }
-  };
+export const fetchHistory = async (): Promise<HistoryItem[]> => {
+  try {
+    const response = await api.get('/history');
+    return response.data;
+  } catch (error) {
+    console.error('Fetch History API Error:', error);
+    throw error;
+  }
+};
