3cd1755db6a725e843ffcbb2df4f1b2efd8947cb
Tal Charnes <tal32123@gmail.com>
2024-06-22 11:26:40 +0300

    feat: make consts on BE

--- backend/src/duckduckgo/duckduckgo.controller.ts
+++ backend/src/duckduckgo/duckduckgo.controller.ts
@@ -5,18 +5,18 @@ import { SearchResponseDto } from './dtos/search-response.dto';
 @Controller('search')
 export class DuckDuckGoController {
   constructor(private readonly duckDuckGoService: DuckDuckGoService) {}
-
+  readonly INITIAL_PAGE: string = "1";
+  readonly DEFAULT_PAGE_SIZE: string = "2";
   @Get()
   async search(
     @Query('q') query: string,
-    @Query('page') page: string = "1",
-    @Query('pageSize') pageSize: string = "2"
+    @Query('page') page: string = this.INITIAL_PAGE,
+    @Query('pageSize') pageSize: string = this.DEFAULT_PAGE_SIZE
   ): Promise<SearchResponseDto[]> {
     let numPageSize = parseInt(pageSize);
     let numPage = parseInt(page);
 
     let result = await this.duckDuckGoService.search(query, numPage, numPageSize);
-    console.log(result);
     return result;
   }
 }
