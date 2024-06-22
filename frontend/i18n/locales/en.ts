13cb87b900193895f8a6f176c5d67719fc4c82be
Tal Charnes <tal32123@gmail.com>
2024-06-22 23:36:01 +0300

    perf: speed up translations and package json run

--- frontend/i18n/locales/en.ts
+++ frontend/i18n/locales/en.ts
@@ -1,12 +1,10 @@
-// src/translations/en.ts
-
-const en = {
+const en: Record<string, string> = {
   DUCK_DUCK_GO_SEARCH: "DuckDuckGo",
   ENTER_SEARCH_QUERY: "Enter search query",
   SEARCH_HISTORY: "Search History",
-  "OCCURRENCES": "Occurrences:",
-  "SEARCH": "Search",
-  "LOADING": "Loading..."
+  OCCURRENCES: "Occurrences:",
+  SEARCH: "Search",
+  LOADING: "Loading...",
 };
 
 export default en;
--- frontend/i18n/useTranslation.ts
+++ frontend/i18n/useTranslation.ts
@@ -1,26 +1,9 @@
-import { useState, useEffect } from 'react';
+import en from "./locales/en";
 
+ 
 const useTranslation = () => {
-    //hardcoded for now
-  const  locale = 'en';
-  const [translations, setTranslations] = useState<Record<string, string>>({});
-
-  useEffect(() => {
-    const loadTranslations = async () => {
-      const currentLocale = locale || 'en';
-      try {
-        const translations = await import(`./locales/${currentLocale}`);
-        setTranslations(translations.default);
-      } catch (error) {
-        console.error(`Error loading translations for locale "${currentLocale}":`, error);
-      }
-    };
-
-    loadTranslations();
-  }, [locale]);
-
   const t = (key: string) => {
-    return translations[key] || key;
+    return en[key] || key;
   };
 
   return { t };
